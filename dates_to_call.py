32b5aff8794fce9fb3c18688c602859d0fdbcf79
alexb <alex.brown577@gmail.com>
2024-12-18 14:15:01 -0500

    Update 3, simplified the process of getting a date list

---
+++ dates_to_call.py
@@ -0,0 +1,34 @@
+import datetime, re
+
+
+def get_dates_to_call_for_year(year, month, day):
+    """
+    takes a year, month, and day, and returns a list of dates, 7 before and 7 after, for that year, accounting for leap year and next/last year
+    :param year:
+    :param month:
+    :param day:
+    :return:
+    """
+    base = datetime.date(year, month, day)
+    date_list = [base - datetime.timedelta(days=x+1) for x in range(-8,7)]
+    date_list_pretty = []
+    for i in date_list:
+        date_list_pretty.append(str(i))
+    return date_list_pretty
+
+
+def generate_master_date_list(month, day):
+    """
+    takes a month and a day and generates a list of days, 7 before and 7 after, for every year back to 1950
+    :param month:
+    :param day:
+    :return: list of lists
+    """
+    current_year = str(datetime.datetime.today())
+    year_only = int(re.findall(r"(\d{,4})-.+", current_year)[0])
+    year_set = range(year_only, 1950, -1)
+    master_date_list = []
+    for year in year_set:
+        single_year_dates = get_dates_to_call_for_year(year, month, day)
+        master_date_list.append(single_year_dates)
+    return master_date_list
