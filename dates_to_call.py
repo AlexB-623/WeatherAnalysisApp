83b89a8c0565b8e3bf1ce014098fe7db31079b73
alexb <alex.brown577@gmail.com>
2024-12-21 09:13:12 -0500

    Update 4, began work on pulling data via API

--- dates_to_call.py
+++ dates_to_call.py
@@ -19,16 +19,20 @@ def get_dates_to_call_for_year(year, month, day):
 
 def generate_master_date_list(month, day):
     """
-    takes a month and a day and generates a list of days, 7 before and 7 after, for every year back to 1950
+    takes a month and a day and generates a list of days,
+    7 before and 7 after, for every year back to 1950
     :param month:
     :param day:
-    :return: list of lists
+    :return: list of dates
     """
     current_year = str(datetime.datetime.today())
     year_only = int(re.findall(r"(\d{,4})-.+", current_year)[0])
+    #do we want to use 1950? may need to look at request limits.
+    #we might be fine with the past 25 years
     year_set = range(year_only, 1950, -1)
     master_date_list = []
     for year in year_set:
         single_year_dates = get_dates_to_call_for_year(year, month, day)
-        master_date_list.append(single_year_dates)
+        for i in single_year_dates:
+            master_date_list.append(i)
     return master_date_list
